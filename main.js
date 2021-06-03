cde19c7161c858aa69bcc1f77c28db55faaad51c
Archianne <helena19w@gmail.com>
2021-06-03 01:13:13 +0100

    rainbow picker

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 const colorPick = document.querySelector("#color");
+const rainbowPick = document.querySelector("#rainbow-button");
+
+let drawing = false;
+let rainbowIsOn = false;
+let hue = 0;
 
 window.addEventListener("load", () => {
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
-
   window.addEventListener("resize", resizeCanvas, false);
 
   //resizing
@@ -13,9 +17,6 @@ window.addEventListener("load", () => {
   }
 
   resizeCanvas();
-
-  //variables
-  let drawing = false;
 
   function draw(e) {
     if (!drawing) {
@@ -29,6 +30,14 @@ window.addEventListener("load", () => {
       ctx.beginPath();
       ctx.moveTo(e.offsetX, e.offsetY);
       ctx.strokeStyle = colorPick.value;
+    }
+
+    if (rainbowIsOn) {
+      hue++;
+      ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+      if (hue >= 360) {
+        hue = 0;
+      }
     }
   }
 
@@ -47,8 +56,13 @@ window.addEventListener("load", () => {
   canvas.addEventListener("mouseout", () => {
     drawing = false;
     ctx.beginPath();
-  })
+  });
 
+  //rainbow button
+  rainbowPick.addEventListener("click", () => {
+    rainbowIsOn = !rainbowIsOn;
+    if (!rainbowIsOn) {
+      ctx.strokeStyle = colorPick.value;
+    }
+  });
 });
-
-// ctx.strokeStyle
