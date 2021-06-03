d67c21655ab9ca87e1fdd66a246ea5e8abf79b62
Archianne <helena19w@gmail.com>
2021-06-03 23:32:19 +0100

    reset button to clean canvas added

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 const colorPick = document.querySelector("#color");
 const rainbowPick = document.querySelector("#rainbow-button");
+const sizePick = document.querySelector("#size");
+const resetCanvas = document.querySelector("#reset-button");
 
 let drawing = false;
 let rainbowIsOn = false;
@@ -9,84 +11,96 @@ let hue = 0;
 let lastX = 0;
 let lastY = 0;
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
   ctx.lineCap = "round";
-  ctx.lineWidth = 5;
-})
+});
 
-  function drawStart(e) {
-    drawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-    drawMove(e);
-  }
+//controls
+let controls = () => {
+  ctx.strokeStyle = colorPick.value;
+  ctx.lineWidth = sizePick.value;
+}
 
-  function drawMove(e) {
-    if (!drawing) {
-      return;
-    } else {
-      ctx.beginPath();
-      ctx.moveTo(lastX, lastY);
-      ctx.lineTo(e.offsetX, e.offsetY);
-      ctx.stroke();
-      [lastX, lastY] = [e.offsetX, e.offsetY];
-      ctx.strokeStyle = colorPick.value;
-    }
+//drawing start, moving, end
+let drawStart = (e) => {
+  drawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  drawMove(e);
+}
 
-    isRainbowActive();
-  }
-
-  function touchMove(e) {
-    e.preventDefault();
-    let touch = e.touches[0];
-    touchX = touch.pageX - touch.target.offsetLeft;
-    touchY = touch.pageY - touch.target.offsetTop;
-
-    if (!drawing) {
-      return;
-    } else {
-      ctx.beginPath();
-      ctx.moveTo(lastX, lastY);
-      ctx.lineTo(touchX, touchY);
-      ctx.stroke();
-      [lastX, lastY] = [touchX, touchY];
-    }
-    isRainbowActive();
-  }
-
-  function drawEnd() {
-    drawing = false;
+let drawMove = (e) => {
+  if (!drawing) {
+    return;
+  } else {
     ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    controls();
   }
 
-  //mouse
-  canvas.addEventListener("mousedown", drawStart);
-  canvas.addEventListener("mousemove", drawMove);
-  canvas.addEventListener("mouseup", drawEnd);
-  canvas.addEventListener("mouseout", drawEnd);
+  isRainbowActive();
+}
 
-  //touchscreen
-  canvas.addEventListener("touchstart", (e) => (drawStart(e.touches[0])), { passive: false });
-  canvas.addEventListener("touchmove", touchMove, { passive: false });
-  canvas.addEventListener("touchend", () => (drawEnd()), false);
+let touchMove = (e) => {
+  e.preventDefault();
+  let touch = e.touches[0];
+  touchX = touch.pageX - touch.target.offsetLeft;
+  touchY = touch.pageY - touch.target.offsetTop;
 
-  //rainbow button
-  rainbowPick.addEventListener("click", () => {
-    rainbowIsOn = !rainbowIsOn;
-    rainbowPick.style.backgroundColor = "red";
-    if (!rainbowIsOn) {
-      ctx.strokeStyle = colorPick.value;
-      rainbowPick.style.backgroundColor = "";
-    }
-  });
+  if (!drawing) {
+    return;
+  } else {
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(touchX, touchY);
+    ctx.stroke();
+    [lastX, lastY] = [touchX, touchY];
+    controls();
+  }
+  isRainbowActive();
+}
 
-  function isRainbowActive() {
-    if (rainbowIsOn) {
-      ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-      hue++;
-      if (hue >= 360) {
-        hue = 0;
-      }
+let drawEnd = () => {
+  drawing = false;
+  ctx.beginPath();
+}
+
+//mouse
+canvas.addEventListener("mousedown", drawStart);
+canvas.addEventListener("mousemove", drawMove);
+canvas.addEventListener("mouseup", drawEnd);
+canvas.addEventListener("mouseout", drawEnd);
+
+//touchscreen
+canvas.addEventListener("touchstart", (e) => (drawStart(e.touches[0])), { passive: false });
+canvas.addEventListener("touchmove", touchMove, { passive: false });
+canvas.addEventListener("touchend", () => (drawEnd()), false);
+
+//rainbow button
+rainbowPick.addEventListener("click", () => {
+  rainbowIsOn = !rainbowIsOn;
+  rainbowPick.style.backgroundColor = "red";
+  if (!rainbowIsOn) {
+    ctx.strokeStyle = colorPick.value;
+    rainbowPick.style.backgroundColor = "";
+  }
+});
+
+let isRainbowActive = () => {
+  if (rainbowIsOn) {
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    hue++;
+    if (hue >= 360) {
+      hue = 0;
     }
   }
+}
+
+//reset canvas
+resetCanvas.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
